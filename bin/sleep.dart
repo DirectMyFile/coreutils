7fdd5bef08ac3d9ceaa8ca25b8653c5ad42e54e3
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-18 01:51:57 -0400

    Sleep

---
+++ bin/sleep.dart
@@ -0,0 +1,31 @@
+import "dart:io";
+
+import "package:coreutils/coreutils.dart";
+
+main(List<String> args) {
+  var result = handleArguments(args, "sleep", usage: "<time>", fail: (result) => result.rest.length != 1);
+  var tstr = result.rest[0];
+
+  var unit = {
+    "s": 1,
+    "m": 60,
+    "h": 120,
+    "d": 86400
+  }[tstr[tstr.length - 1]];
+
+  if (unit == null) {
+    unit = 1;
+  } else {
+    tstr = tstr.substring(0, tstr.length - 1);
+  }
+
+  try {
+    var f = int.parse(tstr);
+
+    var seconds = f * unit;
+    sleep(new Duration(seconds: seconds));
+  } on FormatException catch (e) {
+    print("Invalid Time");
+    exit(1);
+  }
+}
