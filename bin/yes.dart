d156273bf64c9293052517ed8bafc10d9d396ed2
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-19 09:07:34 -0400

    Implement Yes

---
+++ bin/yes.dart
@@ -0,0 +1,17 @@
+import "dart:io";
+
+import "package:coreutils/coreutils.dart";
+
+main(List<String> args) {
+  var opts = handleArguments(args, "yes");
+
+  var m = "y";
+
+  if (opts.rest.isNotEmpty) {
+    m = opts.rest.join(" ");
+  }
+
+  while (true) {
+    print(m);
+  }
+}
