866536537f7238c759a1ecf6349c2ee0d40e14f2
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-19 08:59:01 -0400

    Add env

---
+++ bin/env.dart
@@ -0,0 +1,34 @@
+import "dart:io";
+
+import "package:coreutils/coreutils.dart";
+
+main(List<String> args) async {
+  var opts = handleArguments(args, "env");
+  var eparts = opts.rest.takeWhile((it) => it.contains("=")).toList();
+
+  var hasCommand = opts.rest.length != eparts.length && opts.rest.isNotEmpty;
+
+  var env = new Map.from(Platform.environment);
+  for (var x in eparts) {
+    var name = x.substring(0, x.indexOf("="));
+    var value = x.substring(x.indexOf("=") + 1);
+
+    env[name] = value;
+  }
+
+  if (!hasCommand) {
+    var out = env.keys.map((it) => "${it}=${env[it]}").join("\n");
+    print(out);
+  } else {
+    var cmd = opts.rest.sublist(eparts.length);
+    var exe = cmd.first;
+    cmd = cmd.sublist(1);
+
+    Process process = await Process.start(exe, cmd, workingDirectory: Directory.current.path, environment: env, runInShell: true);
+    stdout.addStream(process.stdout);
+    stderr.addStream(process.stderr);
+    process.stdin.addStream(stdin);
+    var code = await process.exitCode;
+    exit(code);
+  }
+}
